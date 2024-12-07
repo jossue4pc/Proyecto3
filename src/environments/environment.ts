46844c18cf00306f122d049daa8e8a12460469cf
glYohanny <ped.torres@duocuc.cl>
2024-12-06 23:16:35 -0300

    adicion de plugin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export interface Environment {
   production: boolean;
   sendgridApiKey: string;
+
 }
 
 
 export const environment: Environment = {
   production: false,
-  sendgridApiKey: ' '  // Reemplaza esto con tu API key real de SendGrid
+  sendgridApiKey: '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'  // Reemplaza esto con tu API key real de SendGrid
 };
